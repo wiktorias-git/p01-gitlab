8fab8d742b02810404d56f12d30e5ea8e4877cd8
WiktoriaS <wszram@gmail.com>
2020-05-24 18:42:37 +0200

    Czwarty commit

--- 01program.php
+++ 01program.php
@@ -1,5 +1,8 @@
 <?php
 echo("Witaj");
-echo(“<br> Jestem programem 01:”);
+echo("<br> Jestem programem 01");
 echo ("<br> Test");
+echo ("<br> Test 002");
+echo ("<br> Test 003");
+echo ("<br> Dalsza czesc programu");
 ?>
